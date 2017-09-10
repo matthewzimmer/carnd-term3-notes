81b7bf0705301353cbfa9f6d1d3bb7da9cfc881c
Matthew Zimmer <M16r01@gmail.com>
2017-09-09 20:12:56 -0400

    Quiz 10 and 11.

--- quizzes/lesson2/11_print_path.py
+++ quizzes/lesson2/11_print_path.py
@@ -1,17 +1,25 @@
 # -----------
 # User Instructions:
 #
-# Modify the function search so that it returns
-# a table of values called expand. This table
-# will keep track of which step each node was
-# expanded.
+# Modify the the search function so that it returns
+# a shortest path as follows:
 #
-# Make sure that the initial cell in the grid
-# you return has the value 0.
+# [['>', 'v', ' ', ' ', ' ', ' '],
+#  [' ', '>', '>', '>', '>', 'v'],
+#  [' ', ' ', ' ', ' ', ' ', 'v'],
+#  [' ', ' ', ' ', ' ', ' ', 'v'],
+#  [' ', ' ', ' ', ' ', ' ', '*']]
+#
+# Where '>', '<', '^', and 'v' refer to right, left,
+# up, and down motions. Note that the 'v' should be
+# lowercase. '*' should mark the goal cell.
+#
+# You may assume that all test cases for this function
+# will have a path from init to goal.
 # ----------
 
 grid = [[0, 0, 1, 0, 0, 0],
-        [0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
         [0, 0, 1, 0, 1, 0],
         [0, 0, 1, 0, 1, 0],
         [0, 0, 1, 0, 1, 0]]
@@ -19,10 +27,10 @@ init = [0, 0]
 goal = [len(grid)-1, len(grid[0])-1]
 cost = 1
 
-delta = [[-1, 0], # go up
-         [ 0,-1], # go left
-         [ 1, 0], # go down
-         [ 0, 1]] # go right
+delta = [[-1, 0 ], # go up
+         [ 0, -1], # go left
+         [ 1, 0 ], # go down
+         [ 0, 1 ]] # go right
 
 delta_name = ['^', '<', 'v', '>']
 
@@ -31,16 +39,14 @@ def search(grid,init,goal,cost):
     # modify code below
     # ----------------------------------------
     closed = [[0 for col in range(len(grid[0]))] for row in range(len(grid))]
-    expand = [[-1 for col in range(len(grid[0]))] for row in range(len(grid))]
     closed[init[0]][init[1]] = 1
+    expand = [[' ' for col in range(len(grid[0]))] for row in range(len(grid))]
 
     x = init[0]
     y = init[1]
     g = 0
-    e = 0
 
     open = [[g, x, y]]
-    expand[x][y] = e
 
     found = False  # flag that is set when search is complete
     resign = False # flag set if we can't find expand
@@ -48,6 +54,7 @@ def search(grid,init,goal,cost):
     while not found and not resign:
         if len(open) == 0:
             resign = True
+            return 'fail'
         else:
             open.sort()
             open.reverse()
@@ -64,13 +71,12 @@ def search(grid,init,goal,cost):
                     y2 = y + delta[i][1]
                     if x2 >= 0 and x2 < len(grid) and y2 >=0 and y2 < len(grid[0]):
                         if closed[x2][y2] == 0 and grid[x2][y2] == 0:
-                            e = e + cost
                             g2 = g + cost
                             open.append([g2, x2, y2])
                             closed[x2][y2] = 1
-                            expand[x2][y2] = e
-    return expand
 
-expand = search(grid,init,goal,cost)
-for e in expand:
-    print(e)
+    return expand # make sure you return the shortest path
+
+path = search(grid,init,goal,cost)
+for node in path:
+    print(node)
